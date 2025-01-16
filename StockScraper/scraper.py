fa7357a9803f6f4e3d14a000ed3786727bdeca4c
sxmcilrath <samuel.xavier.mac@gmail.com>
2025-01-16 14:12:47 -0600

    very bare bones UI and starting backend

---
+++ StockScraper/scraper.py
@@ -0,0 +1,14 @@
+import requests
+import smtplib
+from selenium import webdriver
+import time
+def main() 
+    #initialize browser
+    driver = webdriver.Chrome() 
+    driver.get(https://www.nasdaq.com/market-activity/stocks/screener)
+    time.sleep(5)
+
+
+
+if __name__ == "__main__":
+    main()
